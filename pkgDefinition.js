c4a82d3dccac9367f4a448fd1ecfd6ee09e3fbf7
Mathieu Schroeter <mathieu@schroetersa.ch>
2014-10-14 16:05:22 +0200

    Fixes pkgDefinition where a config is in the pacman configuration file instead of xcraft.

--- pkgDefinition.js
+++ pkgDefinition.js
@@ -6,12 +6,12 @@ var path      = require ('path');
 var utils     = require ('xcraft-core-utils');
 var zogLog    = require ('xcraft-core-log') (moduleName);
 var xcraftConfig  = require ('xcraft-core-etc').load ('xcraft');
+var pacmanConfig  = require ('xcraft-core-etc').load ('xcraft-contrib-pacman');
 
 exports.load = function (packageName) {
-  var pkgConfig = path.join ( xcraftConfig.pkgProductsRoot,
-                              packageName,
-                              xcraftConfig.pkgCfgFileName
-                            );
+  var pkgConfig = path.join (xcraftConfig.pkgProductsRoot,
+                             packageName,
+                             pacmanConfig.pkgCfgFileName);
 
   var def = utils.yamlFile2Json (pkgConfig);
   zogLog.verb ('JSON output (package):\n' + JSON.stringify (def, null, '  '));
