aca8000191f2c94b3c4c018cc6b458629934f997
Samuel Loup <samuel.loup@gmail.com>
2014-10-14 14:52:25 +0200

    pacman is now using xcraft-core-utils for reading yaml in package def.

--- pkgDefinition.js
+++ pkgDefinition.js
@@ -3,6 +3,7 @@
 var moduleName = 'manager';
 
 var path      = require ('path');
+var utils     = require ('xcraft-core-utils');
 var zogLog    = require ('xcraft-core-log') (moduleName);
 var xcraftConfig  = require ('xcraft-core-etc').load ('xcraft');
 
@@ -12,12 +13,7 @@ exports.load = function (packageName) {
                               xcraftConfig.pkgCfgFileName
                             );
 
-  var yaml = require ('js-yaml');
-  var fs   = require ('fs');
-
-  var data = fs.readFileSync (pkgConfig, 'utf8');
-
-  var def = yaml.safeLoad (data);
+  var def = utils.yamlFile2Json (pkgConfig);
   zogLog.verb ('JSON output (package):\n' + JSON.stringify (def, null, '  '));
 
   return def;
