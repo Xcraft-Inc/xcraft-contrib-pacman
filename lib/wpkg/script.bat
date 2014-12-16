f75b640ea8c3758898da48e9fb1b4a84f4b709c9
Mathieu Schroeter <schroeter@epsitec.ch>
2014-12-16 14:07:17 +0100

    The peon is started by the Makefile generated by CMake when a source package is built.

--- lib/wpkg/script.bat
+++ lib/wpkg/script.bat
@@ -5,6 +5,6 @@ set ACTION=__ACTION__
 set SYSROOT=__SYSROOT__
 set CONFIG=__CONFIG__
 
-for /f "delims=" %%i in ('node -e "process.stdout.write (require ('path').resolve (require ('%SYSROOT%%CONFIG%').bin));"') do set PEON=%%i
+for /f "delims=" %%i in ('node -e "process.stdout.write (require ('path').resolve (__dirname, '%SYSROOT%', require (require ('path').resolve (__dirname, '%SYSROOT%', '%CONFIG%')).bin));"') do set PEON=%%i
 
 node "%PEON%" "%CD%\%SHARE%" %ACTION%
