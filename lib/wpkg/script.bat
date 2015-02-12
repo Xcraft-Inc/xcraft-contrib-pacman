bd975874d6e98a00971652001cef59b1e9ef2058
Mathieu Schroeter <schroeter@epsitec.ch>
2015-02-12 15:59:12 +0100

    Pass the temporary binary directory to the peon.
    On the way, this commit splits the root and the share pass in two
    arguments. Note that this new CMake argument is passed only for
    the makeall hook.

--- lib/wpkg/script.bat
+++ lib/wpkg/script.bat
@@ -4,7 +4,8 @@ set SHARE=__SHARE__
 set ACTION=__ACTION__
 set SYSROOT=__SYSROOT__
 set CONFIG=__CONFIG__
+set CMAKE_BINARY_DIR=%1
 
 for /f "delims=" %%i in ('node -e "process.stdout.write (require ('path').resolve (__dirname, '%SYSROOT%', require (require ('path').resolve (__dirname, '%SYSROOT%', '%CONFIG%')).bin));"') do set PEON=%%i
 
-node "%PEON%" "%CD%\%SHARE%" %ACTION%
+node "%PEON%" "%CD%" "%SHARE%" %ACTION% %CMAKE_BINARY_DIR%
