6eb0526a41f74e16493dc90228600776be523fe3
Mathieu Schroeter <mathieu@schroetersa.ch>
2015-04-03 18:43:54 +0200

    Use xPh with the prerm, postinst and makeall scripts.

--- lib/wpkg/script.bat
+++ lib/wpkg/script.bat
@@ -1,9 +1,9 @@
 @echo off
 
-set SHARE=__SHARE__
-set ACTION=__ACTION__
-set SYSROOT=__SYSROOT__
-set CONFIG=__CONFIG__
+set SHARE=<PACMAN.SHARE>
+set ACTION=<PACMAN.ACTION>
+set SYSROOT=<PACMAN.SYSROOT>
+set CONFIG=<PACMAN.CONFIG>
 set CMAKE_BINARY_DIR=%1
 
 for /f "delims=" %%i in ('node -e "process.stdout.write (require ('path').resolve (__dirname, '%SYSROOT%', require (require ('path').resolve (__dirname, '%SYSROOT%', '%CONFIG%')).bin));"') do set PEON=%%i
