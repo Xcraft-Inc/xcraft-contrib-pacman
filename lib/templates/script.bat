fc0ac8e05915860f8089edb53b7ec7dd083468c8
Mathieu Schroeter <mathieu@schroetersa.ch>
2016-04-23 14:02:34 +0200

    Retrieve the action passed by wpkg in the peon

--- lib/templates/script.bat
+++ lib/templates/script.bat
@@ -6,6 +6,7 @@ set SHARE=<PACMAN.SHARE>
 set ACTION=<PACMAN.ACTION>
 set SYSROOT=<PACMAN.SYSROOT>
 set CONFIG=<PACMAN.CONFIG>
+set WPKGACT="%1"
 if [%1]==[cmake] (
   set CMAKE_BINARY_DIR="%2"
 ) else (
@@ -17,4 +18,4 @@ for /f "delims=" %%i in ('node -e "process.stdout.write (require ('path').resolv
 if [%NAME%]==[] set NAME="%2"
 if [%VERSION%]==[] set VERSION="%3"
 
-node "%PEON%" "%CD%" "%SHARE%" %ACTION% "%CMAKE_BINARY_DIR%" "%NAME%" "%VERSION%"
+node "%PEON%" "%CD%" "%SHARE%" %ACTION% "%WPKGACT%" "%CMAKE_BINARY_DIR%" "%NAME%" "%VERSION%"
