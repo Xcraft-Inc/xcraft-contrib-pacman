c792cd49080158bd9bfbbb0c267983eb3c5c10fc
Mathieu Schroeter <mathieu@schroetersa.ch>
2016-04-23 16:50:47 +0200

    Pass the hook type to the peon
    It will be useful in order to handle the file renaming after a postinst.
    See #156.

--- lib/templates/script.bat
+++ lib/templates/script.bat
@@ -3,6 +3,7 @@
 set NAME=<PACMAN.NAME>
 set VERSION=<PACMAN.VERSION>
 set SHARE=<PACMAN.SHARE>
+set HOOK=<PACMAN.HOOK>
 set ACTION=<PACMAN.ACTION>
 set SYSROOT=<PACMAN.SYSROOT>
 set CONFIG=<PACMAN.CONFIG>
@@ -18,4 +19,4 @@ for /f "delims=" %%i in ('node -e "process.stdout.write (require ('path').resolv
 if [%NAME%]==[] set NAME="%2"
 if [%VERSION%]==[] set VERSION="%3"
 
-node "%PEON%" "%CD%" "%SHARE%" %ACTION% "%WPKGACT%" "%CMAKE_BINARY_DIR%" "%NAME%" "%VERSION%"
+node "%PEON%" "%CD%" "%SHARE%" "%HOOK%" %ACTION% "%WPKGACT%" "%CMAKE_BINARY_DIR%" "%NAME%" "%VERSION%"
