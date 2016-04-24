76b0bf04c0298c5413ac6925197538579bfd03c4
Mathieu Schroeter <mathieu@schroetersa.ch>
2016-04-24 09:37:55 +0200

    Quit with rc 0 the hook script if the peon is not available
    It's the package manager that decides fi the peon must be available or
    not. The hook must just quit properly.

--- lib/templates/script.bat
+++ lib/templates/script.bat
@@ -14,7 +14,9 @@ if [%1]==[cmake] (
   set CMAKE_BINARY_DIR=
 )
 
-for /f "delims=" %%i in ('node -e "process.stdout.write (require ('path').resolve (__dirname, '%SYSROOT%', require (require ('path').resolve (__dirname, '%SYSROOT%', '%CONFIG%')).bin));"') do set PEON=%%i
+if not exist "%CONFIG%" exit 0
+
+for /f "delims=" %%i in ('node -e "process.stdout.write (require ('path').resolve (__dirname, '%SYSROOT%', require (require ('path').resolve (__dirname, '%CONFIG%')).bin));"') do set PEON=%%i
 
 if [%NAME%]==[] set NAME="%2"
 if [%VERSION%]==[] set VERSION="%3"
