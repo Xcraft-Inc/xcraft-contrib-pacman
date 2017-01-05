3dd2b2fe4e38a858fd74fa2912d16f7cea1d2e3a
Mathieu Schroeter <mathieu@schroetersa.ch>
2017-01-05 16:09:15 +0100

    Remove deprecated LD_LIBRARY_PATH stuff

--- pacman.js
+++ pacman.js
@@ -228,7 +228,6 @@ cmd['edit.data'] = function (msg, response) {
   wizard.rulesArgsMakeinstall = def.data.rules.args.makeinstall;
   wizard.deployCmd            = def.data.deploy;
   wizard.registerPath         = def.data.env.path.join (',');
-  wizard.registerLDPath       = def.data.env.ldpath.join (',');
   wizard.embedded             = def.data.embedded;
   if (def.data.runtime) {
     wizard.runtimeConfigureCmd = def.data.runtime.configure;
