06aa1a7ed940588b72becb00253ef4eb86dedca2
Mathieu Schroeter <mathieu@schroetersa.ch>
2019-09-13 16:06:23 +0200

    Pass the SYSROOT and the DISTRIBUTION to the peon

--- lib/templates/script.bat
+++ lib/templates/script.bat
@@ -7,6 +7,7 @@ set HOOK=<PACMAN.HOOK>
 set ACTION=<PACMAN.ACTION>
 set SYSROOT=<PACMAN.SYSROOT>
 set CONFIG=<PACMAN.CONFIG>
+set DISTRIBUTION="%PEON_DISTRIBUTION%"
 set WPKGACT="%1"
 if [%1]==[cmake] (
   set CMAKE_BINARY_DIR="%2"
@@ -21,4 +22,4 @@ for /f "delims=" %%i in ('node -e "process.stdout.write(require('path').resolve(
 if [%NAME%]==[] set NAME="%2"
 if [%VERSION%]==[] set VERSION="%3"
 
-node "%PEON%" "%CD%" "%SHARE%" "%HOOK%" %ACTION% "%WPKGACT%" "%CMAKE_BINARY_DIR%" "%NAME%" "%VERSION%"
+node "%PEON%" "%CD%" "%SHARE%" "%HOOK%" %ACTION% "%WPKGACT%" "%CMAKE_BINARY_DIR%" "%NAME%" "%VERSION%" "%SYSROOT%" "%DISTRIBUTION%"
