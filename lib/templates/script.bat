1c19dc05db9a2e925c96660c47c9e6b0f21eade1
Mathieu Schroeter <mathieu@schroetersa.ch>
2019-09-27 15:29:24 +0200

    Fix bootstrap case where the peon is not installed
    Then the global root must continue with an exit code of 0.

--- lib/templates/script.bat
+++ lib/templates/script.bat
@@ -8,12 +8,19 @@ set ACTION=<PACMAN.ACTION>
 set SYSROOT=<PACMAN.SYSROOT>
 set DISTRIBUTION=<PACMAN.DISTRIBUTION>
 set WPKGACT="%1"
+
+where xcraft-peon.bat >nul 2>nul
+if not errorlevel 0 (
+  if [%HOOK%]==[global] exit 0
+  @echo xcraft-peon is mandatory but not available in PATH
+  exit 1
+)
+
 if [%1]==[cmake] (
   set CMAKE_BINARY_DIR="%2"
 ) else (
   set CMAKE_BINARY_DIR=
 )
-
 if [%NAME%]==[] set NAME="%2"
 if [%VERSION%]==[] set VERSION="%3"
 
